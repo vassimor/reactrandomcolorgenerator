eae4225cd17d8468982b4d1df55cfece4e10c7af
Vasileios Moratis <vassi07@gmail.com>
2026-02-02 20:37:43 +0100

    Basic color functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import logo from './logo.svg';
+import { randomColor } from 'randomcolor';
+import { useState } from 'react';
 
-export default function App() {
+export default  function App() {
+
+const [ newcolor, setNewcolor ]= useState(randomColor());
+
+
+
+
   return (
-    <div className="App">
-     
+
+
+
+    <div className='App' >
+      <h1>React Random Color Generator</h1>
+      <h3>This is a smaller heading</h3>
+      <div style={{backgroundColor:newcolor}}> this is a new div  </div>
+      <p >This is a paragraph{newcolor}</p>
+      <button onClick={ () => setNewcolor(randomColor())}> GENERATE</button>
+      {console.log(newcolor)}
+
+
 
     </div>
   );
